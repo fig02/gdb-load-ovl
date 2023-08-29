be28c78ffd81e26e2e4214cfbb02a085b0c9f0cf
fig02 <fig02srl@gmail.com>
2023-08-29 19:39:24 -0400

    script works

--- gdb_load_ovl.py
+++ gdb_load_ovl.py
@@ -3,6 +3,7 @@
 # Author: Fig
 # Work based off of HailToDodongo's original tool: 
 # https://github.com/HailToDodongo/oot/blob/daea8bacb27425a7dd8923dea6e0467ba68d1047/gdb_script.py
+# Written with the help of Thar0
 # License: CC0 (https://creativecommons.org/publicdomain/zero/1.0/)
 
 import gdb
@@ -10,17 +11,25 @@ import re
 
 TYPE_U32 = gdb.lookup_type('u32')
 
-def GetAllocAddress(table, index):
-    return int(table[index]["loadedRamAddr"].cast(TYPE_U32))
+# address to object-path map, used to unload entire .o files by an address
+obj_address_map = {}
 
-def AddOverlaySymbols(alloc_address):
+def get_section_address(ovl_name, section_name):
+  section_start_name = "_" + ovl_name + "Segment" + section_name + "Start"
+  # 'section_start_name' is a 'text variable' according to gdb, this can not be resolved via python directly
+  return int(gdb.execute('printf "%x", &' + section_start_name, False, True), 16)
+
+def AddOverlaySymbols(overlay_table, index):
+    alloc_address = int(overlay_table[index]["loadedRamAddr"].cast(TYPE_U32))
+
     if alloc_address == 0:
         print("ERROR: Requested overlay is not currently loaded")
         return
     
-    print("Loading symbols for actor at ", alloc_address)
+    vram_address = int(overlay_table[index]["vramStart"].cast(TYPE_U32))
+    
     # get first symbol-name starting from vramStart (usually the first function in the overlay)
-    target_func_name = gdb.execute("info symbol vramStart", False, True).partition(' ')[0].rstrip()
+    target_func_name = gdb.execute(f"info symbol {vram_address}", False, True).partition(' ')[0].rstrip()
 
     # get section in main ELF (returns: "EnKusa_SetupAction in section ..ovl_En_Kusa of zelda_ocarina_mq_dbg.elf")
     ovl_sec_name = gdb.execute("info sym " + target_func_name, False, True)
@@ -41,8 +50,12 @@ def AddOverlaySymbols(alloc_address):
     target_filename = gdb.lookup_symbol(target_func_name)[0].symtab.filename
     obj_name = "build/" + target_filename[:-1] + "o"
 
+    pattern = r'[^/]*$'
+    matches = re.findall(pattern, obj_name)
+    ovl_name = matches[0]
+
     obj_address_map[hex(alloc_address)] = obj_name
-    print("Loading overlay: ", obj_name, "(text:", hex(ovl_offset_text), " data:", hex(ovl_offset_data), " rodata:", hex(ovl_offset_rodata), " bss:", hex(ovl_offset_bss), ")")
+    print("Reading " + ovl_name[:-2] + "...")
 
     gdb.execute("add-symbol-file -readnow " + obj_name +
       " -o 0xFF000000" +
@@ -51,6 +64,8 @@ def AddOverlaySymbols(alloc_address):
       " -s .rodata " + hex(ovl_offset_rodata) +
       " -s .bss "    + hex(ovl_offset_bss),
       False, True)
+    
+    print("Complete.")
 
 class LoadOvlCmd(gdb.Command):
     def __init__(self):
@@ -62,19 +77,12 @@ class LoadOvlCmd(gdb.Command):
         if arg == "PAUSE" or arg == "KALEIDO" or arg == "KALEIDO_SCOPE":
             # Pause menu does not have an enum, special case it. Index is 0 in `gKaleidoMgrOverlayTable`
             table = gdb.lookup_global_symbol("gKaleidoMgrOverlayTable").value()
-            AddOverlaySymbols(GetAllocAddress(table, 0))
+            AddOverlaySymbols(table, 0)
         elif arg == "ACTOR_PLAYER":
             # Player's index does not correspond to his actor ID, Special case it. Index is 1 in `gKaleidoMgrOverlayTable`
             table = gdb.lookup_global_symbol("gKaleidoMgrOverlayTable").value()
-            AddOverlaySymbols(GetAllocAddress(table, 1))
+            AddOverlaySymbols(table, 1)
         else:
-            # try to get the index from the elf via gdb
-            try:
-                index = gdb.lookup_symbol(arg)[0].value().cast(TYPE_U32)
-            except:
-                print("ERROR: Provided enum value could not be found in the elf")
-                return
-
             pattern = r'^([^_]+)'
             matches = re.findall(pattern, arg)
             ovl_type = matches[0]
@@ -86,8 +94,17 @@ class LoadOvlCmd(gdb.Command):
             elif ovl_type == "EFFECT":
                 table = gdb.lookup_global_symbol("gEffectSsOverlayTable").value()
             else:
-                raise Exception("Type of enum provided is not supported")
+                print("ERROR: Type of enum provided is not supported")
+                return
+            
+            # try to get the index from the elf via gdb
+            try:
+                index = gdb.lookup_symbol(arg)[0].value().cast(TYPE_U32)
+            except:
+                print("ERROR: Provided enum value could not be found in the elf")
+                return
 
-            AddOverlaySymbols(GetAllocAddress(table, index))
+
+            AddOverlaySymbols(table, index)
 
 LoadOvlCmd()
